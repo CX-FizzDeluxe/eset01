02cd5b3db0d90e7f8a41a78e7e392892148d8c04
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-31 14:57:19 +0000

    Tue Oct 31 14:57:19 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -3,6 +3,10 @@ package befaster.solutions;
 import befaster.runner.SolutionNotImplementedException;
 
 public class FizzBuzz {
+	
+	public static final String FIZZ = "fizz";
+	public static final String BUZZ = "buzz";
+	public static final String FIZZBUZZ = "fizz buzz";
 
     public static String fizzBuzz(Integer number) {
         throw new SolutionNotImplementedException();
---
+++ src/test/java/befaster/solutions/FizzBuzzTest.java
@@ -0,0 +1,16 @@
+package befaster.solutions;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
