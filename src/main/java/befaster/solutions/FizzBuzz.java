1c2196f3122f28afc02ec9d78023d3ded7ffdf72
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-31 15:06:04 +0000

    Tue Oct 31 15:06:04 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -9,7 +9,23 @@ public class FizzBuzz {
 	public static final String FIZZBUZZ = "fizz buzz";
 
     public static String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+    	String output = "";
+    	
+        if (number % 3 == 0) {
+        	output += FIZZ;
+        }
+        
+        if (number % 5 == 0) {
+        	output += BUZZ;
+        }
+        
+        if (output.equals(FIZZ+BUZZ)) {
+        	output = FIZZBUZZ;
+        } else if (output.equals("")) {
+        	output = number.toString();
+        }
+        
+        return output;
     }
 
 }
