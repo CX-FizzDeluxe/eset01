eeeb66b564ca25ba2e320ba3e550e00f35e73060
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-31 14:47:32 +0000

    Tue Oct 31 14:47:32 GMT 2017

--- src/test/java/befaster/solutions/HelloTest.java
+++ src/test/java/befaster/solutions/HelloTest.java
@@ -1,14 +1,27 @@
 package befaster.solutions;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Test;
 
 public class HelloTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void helloWorldTest() {
+		assertThat(Hello.hello("world"), equalTo("Hello world"));
+	}
+	
+	@Test
+	public void helloStrangerTest() {
+		assertThat(Hello.hello("stranger"), equalTo("Hello stranger"));
+	}
+	
+	@Test
+	public void helloNooneTest() {
+		assertThat(Hello.hello(""), equalTo("Hello world"));
+		assertThat(Hello.hello(null), equalTo("Hello world"));
 	}
 
 }
