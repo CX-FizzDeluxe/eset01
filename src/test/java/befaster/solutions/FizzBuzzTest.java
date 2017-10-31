2101cc9f99f1b75b2c091430fea5589e4586a48f
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-31 15:00:19 +0000

    Tue Oct 31 15:00:19 GMT 2017

--- src/test/java/befaster/solutions/FizzBuzzTest.java
+++ src/test/java/befaster/solutions/FizzBuzzTest.java
@@ -7,10 +7,36 @@ import static org.hamcrest.Matchers.equalTo;
 import org.junit.Test;
 
 public class FizzBuzzTest {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMultipleOfThree() {
+		assertThat(FizzBuzz.fizzBuzz(3), equalTo(FizzBuzz.FIZZ));
+		assertThat(FizzBuzz.fizzBuzz(27), equalTo(FizzBuzz.FIZZ));
+		assertThat(FizzBuzz.fizzBuzz(63), equalTo(FizzBuzz.FIZZ));
+		assertThat(FizzBuzz.fizzBuzz(9999), equalTo(FizzBuzz.FIZZ));
+	}
+	
+	
+	@Test
+	public void testMultipleOfFive() {
+		assertThat(FizzBuzz.fizzBuzz(5), equalTo(FizzBuzz.BUZZ));
+		assertThat(FizzBuzz.fizzBuzz(50), equalTo(FizzBuzz.BUZZ));
+		assertThat(FizzBuzz.fizzBuzz(65), equalTo(FizzBuzz.BUZZ));
+		assertThat(FizzBuzz.fizzBuzz(9995), equalTo(FizzBuzz.BUZZ));
+	}
+	
+	@Test
+	public void testMultipleOfBoth() {
+		assertThat(FizzBuzz.fizzBuzz(15), equalTo(FizzBuzz.FIZZBUZZ));
+		assertThat(FizzBuzz.fizzBuzz(60), equalTo(FizzBuzz.FIZZBUZZ));
+		assertThat(FizzBuzz.fizzBuzz(120), equalTo(FizzBuzz.FIZZBUZZ));
+	}
+	
+	@Test
+	public void testNotMultiple() {
+		assertThat(FizzBuzz.fizzBuzz(1), equalTo("1"));
+		assertThat(FizzBuzz.fizzBuzz(9998), equalTo("9998"));
+		assertThat(FizzBuzz.fizzBuzz(37), equalTo("37"));
 	}
 
 }
