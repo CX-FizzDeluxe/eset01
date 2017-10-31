6e0c1be40231c8940eadc4d30250c313bd0de465
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-31 15:13:20 +0000

    Tue Oct 31 15:13:20 GMT 2017

--- src/test/java/befaster/solutions/FizzBuzzTest.java
+++ src/test/java/befaster/solutions/FizzBuzzTest.java
@@ -9,20 +9,42 @@ import org.junit.Test;
 public class FizzBuzzTest {
 	
 	@Test
-	public void testMultipleOfThree() {
-		assertThat(FizzBuzz.fizzBuzz(3), equalTo(FizzBuzz.FIZZ));
+	public void testMultipleOfThreeButNotContaining() {
 		assertThat(FizzBuzz.fizzBuzz(27), equalTo(FizzBuzz.FIZZ));
-		assertThat(FizzBuzz.fizzBuzz(63), equalTo(FizzBuzz.FIZZ));
 		assertThat(FizzBuzz.fizzBuzz(9999), equalTo(FizzBuzz.FIZZ));
 	}
 	
+	@Test
+	public void testMultipleOfThreeAndContaining() {
+		assertThat(FizzBuzz.fizzBuzz(3), equalTo(FizzBuzz.FIZZ));
+		assertThat(FizzBuzz.fizzBuzz(63), equalTo(FizzBuzz.FIZZ));
+	}
 	
 	@Test
-	public void testMultipleOfFive() {
+	public void testContainsThreeButNotMultiple() {
+		assertThat(FizzBuzz.fizzBuzz(13), equalTo(FizzBuzz.FIZZ));
+		assertThat(FizzBuzz.fizzBuzz(37), equalTo(FizzBuzz.FIZZ));
+		assertThat(FizzBuzz.fizzBuzz(103), equalTo(FizzBuzz.FIZZ));
+	}
+	
+	@Test
+	public void testMultipleOfFiveButNotContaining() {
+		// this can't be tested because all multiples of 5 contain 5
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testMultipleOfFiveAndContaining() {
 		assertThat(FizzBuzz.fizzBuzz(5), equalTo(FizzBuzz.BUZZ));
 		assertThat(FizzBuzz.fizzBuzz(50), equalTo(FizzBuzz.BUZZ));
 		assertThat(FizzBuzz.fizzBuzz(65), equalTo(FizzBuzz.BUZZ));
 		assertThat(FizzBuzz.fizzBuzz(9995), equalTo(FizzBuzz.BUZZ));
+	}
+	
+	@Test
+	public void testContainsFiveButNotMultiple() {
+		assertThat(FizzBuzz.fizzBuzz(56), equalTo(FizzBuzz.BUZZ));
+		assertThat(FizzBuzz.fizzBuzz(7751), equalTo(FizzBuzz.BUZZ));
 	}
 	
 	@Test
@@ -33,10 +55,10 @@ public class FizzBuzzTest {
 	}
 	
 	@Test
-	public void testNotMultiple() {
+	public void testNotMultipleNotContaining() {
 		assertThat(FizzBuzz.fizzBuzz(1), equalTo("1"));
 		assertThat(FizzBuzz.fizzBuzz(9998), equalTo("9998"));
-		assertThat(FizzBuzz.fizzBuzz(37), equalTo("37"));
+		assertThat(FizzBuzz.fizzBuzz(47), equalTo("47"));
 	}
 
 }
