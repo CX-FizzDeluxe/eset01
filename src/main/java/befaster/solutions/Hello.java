e0d71e269391bdbcffe6fd8b734025eeed7648f4
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-31 14:50:07 +0000

    Tue Oct 31 14:50:07 GMT 2017

--- src/main/java/befaster/solutions/Hello.java
+++ src/main/java/befaster/solutions/Hello.java
@@ -1,7 +1,15 @@
 package befaster.solutions;
 
+import org.apache.commons.codec.binary.StringUtils;
+
 public class Hello {
     public static String hello(String friendName) {
+    	if (friendName == null || friendName.trim().equals("")) {
+    		friendName = "world";
+    	} else {
+    		friendName = friendName.trim();
+    	}
+    	
         return "Hello " + friendName;
     }
 }
--- src/test/java/befaster/solutions/HelloTest.java
+++ src/test/java/befaster/solutions/HelloTest.java
@@ -11,16 +11,19 @@ public class HelloTest {
 	@Test
 	public void helloWorldTest() {
 		assertThat(Hello.hello("world"), equalTo("Hello world"));
+		assertThat(Hello.hello(" world   "), equalTo("Hello world"));
 	}
 	
 	@Test
 	public void helloStrangerTest() {
 		assertThat(Hello.hello("stranger"), equalTo("Hello stranger"));
+		assertThat(Hello.hello("  stranger   "), equalTo("Hello stranger"));
 	}
 	
 	@Test
 	public void helloNooneTest() {
 		assertThat(Hello.hello(""), equalTo("Hello world"));
+		assertThat(Hello.hello("   "), equalTo("Hello world"));
 		assertThat(Hello.hello(null), equalTo("Hello world"));
 	}
 
